a78b975a6ae8d03d4c6f4cc46e35834b461f3764
Arthur KIENTZLER <147141390+arthurklr@users.noreply.github.com>
2024-09-12 11:47:30 +0200

    Affichage de toutes les infos

--- vue/vueCommande.php
+++ vue/vueCommande.php
@@ -8,18 +8,22 @@ ob_start();
 <div class='resultat'>
   <div class="titreCommande">Client :</div>
   <?= $client[0]['nom'] . " " . $client[0]['prenom'] ?>
+  <br>
+  <?= $client[0]['adresse'] ?>
+  <br>
+  <?= $client[0]['ville'] ?>
   <div class="titreCommande">Articles :</div>
   <?php
-  if (count($article)) {
+  if (count($articles)) {
     // Affichage des titres de colonnes du tableau
     echo '<table><tr>';
-    foreach ($article[0] as $cle => $valeur) {
+    foreach ($articles[0] as $cle => $valeur) {
       echo '<th>' . $cle . '</th>';
     }
     echo '</tr>';
 
     // Affichage des lignes du tableau
-    foreach ($article as $ligne) {
+    foreach ($articles as $ligne) {
       echo '<tr>';
       // Affichage des valeurs d'une ligne
       foreach ($ligne as $valeur) {
@@ -29,7 +33,7 @@ ob_start();
     }
     echo '</table>';
   } else
-    echo "<div class='reponse'>Aucun client n'est enregistré dans la liste</div>";
+    echo "<div class='reponse'>Aucun article n'est enregistré dans la liste</div>";
   ?>
   <div class="titreCommande">Total : <?= $total ?> €</div>
 </div>
